3f135f9b102e02f0817871b72ed51eee82bf7b9c
manali shah <manalishah133@gmail.com>
2021-04-14 13:15:27 +0530

    First Commit

--- src/navbar.js
+++ src/navbar.js
@@ -7,10 +7,7 @@ function navbar() {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <Link className="navbar-brand" to="/">Banking</Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div >
                     <div className="navbar-nav ">
                         {/* <Link className="nav-link " to="/">Home <span class="sr-only">(current)</span></Link> */}
                         <Link className="nav-link" to="/Customer">Customers</Link>
